f66706edf2e7e7e3f3d229628b2e072a2e57cdf7
kukulee-git <2074805901@qq.com>
2026-05-08 00:08:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ from pylab import*
 mpl.rcParams['font.sans-serif']=['SimHei']
 
 import matplotlib.pyplot as plt
-plt.rcParams['font.sans-serif'] = ['SimHei']
-plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['font.family'] = 'Heiti TC'  # 替换为你选择的字体
 
 from sklearn.ensemble import IsolationForest, RandomForestRegressor
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
